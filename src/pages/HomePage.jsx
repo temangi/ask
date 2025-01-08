ab3dc82bf5c5a1dcb94d44d81e38b6a9aeeaab33
Aidana <emajdana01@gmail.com>
2025-01-08 16:58:10 +0600

    feat  footer

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,11 @@
+import Footer from "../components/Footer/Footer"
+
 function HomePage(){
     return(
         <>
         <h1>Home Page</h1>
         <p>ADIL , AIDANA, ALINA</p>
+        <Footer/>
         </>
     )
 }
