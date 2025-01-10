cf183915f6471a0de58e5d3867bb86cc9b5c86ab
Адилья <adilik1505@gmail.com>
2025-01-10 17:38:36 +0600

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import ServicePage from './pages/servicePage'
 
+
 function App() {
   return (
    <>
