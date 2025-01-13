0b145ce40ff44fead8d67d18c414d97f96cc4ed1
Aidana <emajdana01@gmail.com>
2025-01-13 13:37:54 +0600

    feat bloah

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,17 @@
-import Footer from "../components/Footer/Footer"
-import Header from "../components/Header/Heade"
+import Footer from "../components/Footer/Footer";
+import Header from "../components/Header/Heade";
+import Blog from "../components/Blog/Blog";
 
-function HomePage(){
-    return(
-        <>
-        <h1>Home Page</h1>
-        <p>ADIL , AIDANA, ALINA</p>
-        <Header/>
-      <Footer/>
-        </>
-    )
+function HomePage() {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <p>ADIL , AIDANA, ALINA</p>
+      <Header />
+      <Blog />
+      <Footer />
+    </>
+  );
 }
 
-export default HomePage
+export default HomePage;
