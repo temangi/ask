158761aec0e98a7c6123f60e229bc60103809a8f
temangi <temirlan2.nuri@gmail.com>
2025-01-15 16:54:58 +0600

    fix

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from '../components/Header/Heade'
 import Footer from '../components/Footer/Footer'
 import Main from '../components/Main/Main'
+import Client from '../components/Clientt/Client'
 
 function AboutPage() {
   return (
     <section>
       <Header/>
       <Main/>
+      <Client/>
       <Footer/>
     </section>
   )
